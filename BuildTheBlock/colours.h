1c277b0e465aed2993484a43429043d7e8b527b4
Chronoline <chaajaka@hotmail.com>
2025-06-06 22:47:43 +0100

    Initial rendering of game grid, constants file, custom colours file

---
+++ BuildTheBlock/colours.h
@@ -0,0 +1,11 @@
+#ifndef COLOURS_H
+#define COLOURS_H
+
+#include "raylib.h"
+
+namespace customColours
+{
+	extern Color backgroundGreen;
+}
+
+#endif
---
+++ BuildTheBlock/constants.cpp
@@ -0,0 +1,8 @@
+#include "constants.h"
+
+namespace constants
+{
+	const int screenWidth = 1600;
+	const int screenHeight = 800;
+	const int gridCellSize = 20;
+}
